37acafad8d9886c6d0799c0d5b7ea9a56123a427
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-04-13 19:13:03 +0200

    enter link test

--- packages/e2e/features/home.ts
+++
@@ -1,8 +0,0 @@
-describe("Home page", () => {
-    it("should print my name", () => {
-        cy.visit("/");
-
-        cy.title().should("equal", "Alexandre Djerbetian");
-        cy.contains("Alexandre Djerbetian");
-    });
-});

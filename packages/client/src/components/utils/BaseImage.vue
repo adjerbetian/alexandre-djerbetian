0754fabae98c6bcc5c8c4b1b96654c283aec2adc
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-04-25 20:13:50 +0200

    extract BaseImage

---
+++ packages/client/src/components/utils/BaseImage.vue
@@ -0,0 +1,22 @@
+<!--  See https://blog.lichter.io/posts/dynamic-images-vue-nuxt/ -->
+<template>
+    <img :src="src" :alt="alt" />
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { imageService } from "@/services";
+
+@Component
+export default class BaseImage extends Vue {
+    @Prop(String) type!: string;
+    @Prop(String) alt!: string;
+    @Prop(String) name!: string;
+
+    get src() {
+        return imageService.getImage(this.type, this.name);
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>

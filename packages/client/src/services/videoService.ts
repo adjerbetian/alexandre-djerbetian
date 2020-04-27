78b38e2130e8923045b5cab622389af6aa7e3e51
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-04-27 12:19:35 +0200

    added asEntity

--- packages/client/src/services/videoService.ts
+++ packages/client/src/services/videoService.ts
@@ -1,7 +1,7 @@
-import { buildVideo, Video } from "entities";
+import { buildVideo, VideoDTO } from "entities";
 import { api } from "@/services/api";
 
 export async function fetchAllVideos() {
-    const dtos: Video[] = await api.get("/videos");
+    const dtos: VideoDTO[] = await api.get("/videos");
     return dtos.map((dto) => buildVideo(dto));
 }
---
+++ packages/entities/src/entity.ts
@@ -0,0 +1,7 @@
+export function asEntityBuilder<DTO, Entity extends DeepReadonly<DTO>>(builder: (dto: DTO) => Entity) {
+    return builder;
+}
+
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
+};

132a5e15db80158530ea07545f8347e976abb18f
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-02 19:56:37 +0200

    extract BookCover

---
+++ packages/client/src/books/Cover.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="cover">
+        <img :src="cover" :alt="`cover of ${book.title}`" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { Book } from "entities";
+import { imageService } from "@/utils";
+
+@Component
+export default class BookCover extends Vue {
+    @Prop(Object) book!: Book;
+
+    get cover(): string {
+        return imageService.getImage("books", this.book.id);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/variables";
+
+.cover {
+    img {
+        display: block;
+    }
+}
+</style>

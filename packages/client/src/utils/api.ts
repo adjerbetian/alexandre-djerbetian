a94f313250dcc292922b35f93adcdbc4cdf26bc6
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-10 11:52:32 +0200

    add ping on app startup

--- packages/client/src/utils/api.ts
+++ packages/client/src/utils/api.ts
@@ -1,10 +1,18 @@
 const serverUrl = process.env.VUE_APP_SERVER;
 
 export async function get(route: string) {
+    const response = await doFetch(route);
+    return response.json();
+}
+export async function ping(route: string) {
+    await doFetch(route);
+}
+
+async function doFetch(route: string) {
     const response = await fetch(serverUrl + route);
     if (response.status !== 200) {
         console.log(response);
         throw new Error(`Error while fetching the server: ${response.status}`);
     }
-    return response.json();
+    return response;
 }

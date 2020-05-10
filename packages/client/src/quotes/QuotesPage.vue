5b0e4787b9a6371aaa28fc976f5d79290f2c23d9
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-10 17:08:12 +0200

    first version of checkbox

--- packages/client/src/quotes/QuotesPage.vue
+++ packages/client/src/quotes/QuotesPage.vue
@@ -1,25 +1,72 @@
 <template>
     <div>
         <h1>Some quotes</h1>
+
+        <div class="filters">
+            <h2>Filters</h2>
+            <div class="filters-group">
+                <h3>Books</h3>
+                <div class="filters-list">
+                    <label
+                        v-for="book in books"
+                        :key="book.id"
+                        :title="`${book.title} - ${book.authors.join(', ')} - ${book.releaseYear}`"
+                    >
+                        <input
+                            type="checkbox"
+                            v-model="bookFilters"
+                            :value="book.id"
+                            @change="updateSearch"
+                        />
+                        {{ book.title }}
+                    </label>
+                </div>
+            </div>
+        </div>
+
         <QuoteComponent v-for="quote in quotes" :key="quote.id" :quote="quote" />
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import _ from "lodash";
 import { Component } from "vue-property-decorator";
-import { Quote } from "@alex/entities";
-import { fetchAll } from "./quoteService";
+import { Book, Quote } from "@alex/entities";
+import * as quoteService from "./quoteService";
+import { bookService } from "@/books";
 import QuoteComponent from "./Quote.vue";
 
 @Component({ components: { QuoteComponent } })
 export default class QuotesPage extends Vue {
     quotes: Quote[] = [];
+    books: Book[] = [];
+    bookFilters: string[] = [];
 
     async mounted() {
-        this.quotes = await fetchAll();
+        this.initFiltersFromSearchQuery();
+        this.quotes = await quoteService.fetchAll();
+        this.books = await bookService.fetchAll();
+
+        this.$watch("$route.query.books", this.initFiltersFromSearchQuery);
+    }
+    initFiltersFromSearchQuery() {
+        this.bookFilters.length = 0;
+        this.bookFilters.push(...toArray(this.$route.query.books));
+    }
+    async updateSearch() {
+        await this.$router.push({ query: { books: this.bookFilters } });
     }
 }
+
+function toArray(query: SearchQuery): string[] {
+    if (!query) return [];
+    if (typeof query === "string") return [query];
+
+    return query.map((q) => q || "").filter((q) => !_.isEmpty(q));
+}
+
+type SearchQuery = string | (string | null)[];
 </script>
 
 <style lang="scss" scoped>
--- packages/e2e/features/quotes.ts
+++ packages/e2e/features/quotes.ts
@@ -20,12 +20,81 @@ describe("Quotes page", () => {
     it("should print the quote's caption", () => {
         cy.contains("Clean code reads like well-written prose.")
             .closest(".quote")
-            .within(() => {
-                cy.contains("Clean Code - chapter 1 - Clean Code, p8");
-            });
+            .within(() =>
+                cy.contains("Clean Code - chapter 1 - Clean Code, p8")
+            );
     });
     it("should include the best quotes first (and not print the less good quotes)", () => {
         cy.contains("Clean code reads like well-written prose.");
-        cy.contains("There are two parts to learning craftsmanship: knowledge and work.").should("not.exist");
+        cy.contains(
+            "There are two parts to learning craftsmanship: knowledge and work."
+        ).should("not.exist");
+    });
+    describe("book filters", () => {
+        it("should have one filter per book", () => {
+            cy.contains("div", "Filters").within(() => {
+                cy.contains("Clean Code");
+                cy.contains("The Clean Coder");
+                cy.contains("Clean Architecture");
+                cy.contains("Refactoring");
+                cy.contains("The Mythical Man-Month");
+            });
+        });
+        it("should check the filter when clicking on the filter", () => {
+            clickOnFilter("Refactoring");
+            checkboxFor("refactoring").should("be.checked");
+
+            clickOnFilter("Refactoring");
+            checkboxFor("refactoring").should("not.be.checked");
+        });
+        // prettier-ignore
+        it("should change the url when filtering on a book", () => {
+            clickOnFilter("Refactoring");
+            cy.location("search").should("equal", "?books=refactoring");
+
+            clickOnFilter("Clean Architecture");
+            cy.location("search").should("equal", "?books=refactoring&books=clean-architecture");
+
+            clickOnFilter("Refactoring");
+            cy.location("search").should("equal", "?books=clean-architecture");
+
+            clickOnFilter("Clean Architecture");
+            cy.location("search").should("be.empty");
+        });
+        it("should check the right filters when loading a page with a book query", () => {
+            cy.visit("/quotes?books=refactoring&books=clean-architecture");
+
+            checkboxFor("refactoring").should("be.checked");
+            checkboxFor("clean-architecture").should("be.checked");
+            checkboxFor("clean-code").should("not.be.checked");
+        });
+        it("should uncheck/recheck the right filter when navigating back and forth", () => {
+            clickOnFilter("Clean Architecture");
+            checkboxFor("clean-architecture").should("be.checked");
+
+            cy.go("back");
+            checkboxFor("clean-architecture").should("not.be.checked");
+
+            cy.go("forward");
+            checkboxFor("clean-architecture").should("be.checked");
+        });
+        it.skip("should not show quotes from Clean Code when filtering for Refactoring", () => {
+            cy.contains("Clean Code - chapter 1");
+
+            cy.contains("div", "Filters").within(() =>
+                cy.contains("Refactoring").click()
+            );
+
+            cy.contains("Clean Code - chapter 1").should("not.exist");
+        });
+
+        function clickOnFilter(name: string) {
+            cy.contains("div", "Filters").within(() =>
+                cy.contains(name).click()
+            );
+        }
+        function checkboxFor(name: string) {
+            return cy.get(`input[value=${name}]`);
+        }
     });
 });

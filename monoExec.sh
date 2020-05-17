b176fa6d15efc8f6d70d669b91417417723384e9
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-17 17:55:07 +0200

    deploy

--- monoExec.sh
+++ monoExec.sh
@@ -9,7 +9,7 @@ purple="\\033[35m"
 bold="\\033[1m"
 reset="\\033[0m"
 
-printFolderCMD="echo \\${blue}\\${bold}\$(basename \$(pwd))\\${reset}"
+printFolderCMD="echo -e \\${blue}\\${bold}\$(basename \$(pwd))\\${reset}"
 
 echo -e "${bold}${purple}${command}${reset}"
 lerna exec --loglevel silent ${options} -- "${printFolderCMD} && ${command}"
--- monoRun.sh
+++ monoRun.sh
@@ -9,7 +9,7 @@ purple="\\033[35m"
 bold="\\033[1m"
 reset="\\033[0m"
 
-printFolderCMD="echo \\${blue}\\${bold}\$(basename \$(pwd))\\${reset}"
+printFolderCMD="echo -e \\${blue}\\${bold}\$(basename \$(pwd))\\${reset}"
 
 echo -e "${bold}${purple}${script}${reset}"
 lerna exec --loglevel silent ${options} -- "${printFolderCMD} && ((yarn run --non-interactive | grep -q ${script}) && yarn ${script} || echo 'no script ${script}')"

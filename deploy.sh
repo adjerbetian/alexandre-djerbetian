20fc145f108aa88483d2e985d4a40a5bb04a65c4
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-17 17:55:07 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,49 @@
 #!/usr/bin/env bash
 set -e
 
-sync() {
-    from=$1
-    to=$2
-
-    rsync -avz --delete --copy-links -e "ssh -p 5022" "${from}" "${HOST}:sites/alexandre-new/${to}"
-}
-
-yarn compile
-(cd packages/client && NODE_ENV=prod;VUE_APP_SERVER=https://api.djerbetian.com yarn build)
-
 USER=djerbeti
 IP=world-312.fr.planethoster.net
 HOST="${USER}@${IP}"
 
-sync "packages/client/dist/" "client"
-sync "packages/server/dist/" "server/dist"
-sync "packages/server/node_modules/" "server/node_modules"
+syncAllDist() {
+    sync micro
+    sync entities
+    sync db
+    sync server
+    sync client
+}
+syncDist() {
+    sync "packages/$1/dist"
+}
+sync() {
+    folder=$1
+    rsync -avz --delete --copy-links -e "ssh -p 5022" "${folder}/" "${HOST}:sites/alexandre/${folder}"
+}
 
-restartPath="/home/djerbeti/sites/alexandre-new/server/dist/tmp/restart.txt"
-ssh -p 5022 ${HOST} "mkdir -p \$(dirname ${restartPath}) touch ${restartPath}"
+buildClient() {
+    yarn bootstrap
+    (cd packages/client && NODE_ENV=prod;VUE_APP_SERVER=https://api.djerbetian.com npm run build)
+}
+
+pullInstallOnServer() {
+    ssh -p 5022 "${HOST}" << INSTALL
+        source /home/djerbeti/nodevenv/sites/alexandre/10/bin/activate
+        cd /home/djerbeti/sites/alexandre
+        git checkout master
+        git pull
+        npx yarn install --frozen-lockfile --silent
+INSTALL
+}
+
+restart() {
+    ssh -p 5022 "${HOST}" << RESTART
+        touch /home/djerbeti/sites/alexandre/tmp/restart.txt
+RESTART
+}
+
+# ---------------------------------------------
+
+buildClient
+pullInstallOnServer
+syncAllDist
+restart

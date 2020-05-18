087f82bfc59acb51679a2300d4f5b54825b6a46f
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-18 22:52:24 +0200

    simplify monoRun.sh

--- monoRun.sh
+++ monoRun.sh
@@ -4,12 +4,4 @@ set -e
 script=$1
 options="$2 $3 $4 $5"
 
-blue="\\033[34m"
-purple="\\033[35m"
-bold="\\033[1m"
-reset="\\033[0m"
-
-printFolderCMD="echo -e \\${blue}\\${bold}\$(basename \$(pwd))\\${reset}"
-
-echo -e "${bold}${purple}${script}${reset}"
-lerna exec --loglevel silent ${options} -- "${printFolderCMD} && ((yarn run --non-interactive | grep -q ${script}) && yarn ${script} || echo 'no script ${script}')"
+./monoExec.sh "npm run --if-present ${script}" ${options}

8665216ebe5e1c0059fe915a71d8952ed01fd91f
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-23 13:56:52 +0200

    add code coloration

--- packages/db/src/books/utils.ts
+++ packages/db/src/books/utils.ts
@@ -1,18 +1,20 @@
 import { µ } from "@alex/micro";
+import hljs from "highlight.js";
 
 export function java(content: string) {
-    return code("java", content);
+    return code(content, "java");
 }
 export function cpp(content: string) {
-    return code("cpp", content);
+    return code(content, "cpp");
 }
 export function javascript(content: string) {
-    return code("javascript", content);
+    return code(content, "javascript");
 }
 
-function code(lang: string, content: string) {
-    // prettier-ignore
-    return `
-        <pre><code class="langage-${lang}">${µ.trimCommonIndentation(content)}</code></pre>
-    `;
+function code(content: string, lang: string) {
+    content = colorize(µ.trimCommonIndentation(content), lang);
+    return `<pre class="code light"><code class="hljs language-${lang}">${content}</code></pre>`;
+}
+function colorize(content: string, lang: string) {
+    return hljs.highlight(lang, content).value;
 }

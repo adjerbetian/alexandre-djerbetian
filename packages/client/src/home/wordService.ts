94576ff2661c9025be833de454517954331ba445
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-31 12:06:20 +0200

    add blur on home image sides

--- packages/client/src/home/wordService.ts
+++ packages/client/src/home/wordService.ts
@@ -1,4 +1,4 @@
-let id = 0;
+let counter = 0;
 
 const WORDS = [
     "Design Patterns",
@@ -17,31 +17,50 @@ interface Word {
     id: number;
     content: string;
     position: {
-        left: boolean;
+        right: boolean;
         y: number;
     };
+}
+interface Position {
+    right: boolean;
+    yMin: number;
+    yMax: number;
 }
 
 export const words = {
     all: [] as Word[],
-    addWord() {
-        words.all.push(generateWord());
+    addWord({ right, yMin, yMax }: Position) {
+        const id = counter++;
+        words.all.push({
+            id,
+            position: {
+                right,
+                y: generateBetween(yMin, yMax, {
+                    excludes: words.all.map((w) => w.position.y),
+                    distance: 6
+                })
+            },
+            content: WORDS[counter % WORDS.length]
+        });
     },
     removeWord() {
         words.all.shift();
     }
 };
 
-function generateWord(): Word {
-    return {
-        id: id++,
-        content: WORDS[id % WORDS.length],
-        position: {
-            left: Math.random() > 0.5,
-            y: generateBetween(20, 60)
-        }
-    };
+function generateBetween(
+    min: number,
+    max: number,
+    { excludes, distance }: { excludes: number[]; distance: number }
+) {
+    let result: number;
+    let n = 0;
+    do {
+        result = Math.random() * (max - min) + min;
+        n++;
+    } while (n < 10 && excludes.some((v) => d(v, result) < distance));
+    return result;
 }
-function generateBetween(min: number, max: number) {
-    return Math.random() * (max - min) + min;
+function d(a: number, b: number) {
+    return Math.abs(a - b);
 }

f841b1d592b29b88167f1369dace515f1e3df003
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-05-31 12:06:20 +0200

    add blur on home image sides

--- packages/client/src/home/HomePage.vue
+++ packages/client/src/home/HomePage.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="home">
+        <div class="home-background"></div>
         <transition-group name="fade">
             <div
                 class="word"
                 v-for="word in words"
+                :class="{ right: word.position.right }"
                 :key="word.id"
                 :style="{
-                    [word.position.left ? `left` : `right`]: `50px`,
                     top: `${word.position.y}%`
                 }"
             >
@@ -33,7 +34,11 @@ export default class HomePage extends Vue {
         setInterval(this.printWord, DELAY);
     }
     printWord() {
-        words.addWord();
+        words.addWord({
+            right: Math.random() > 0.5,
+            yMin: 10,
+            yMax: 70
+        });
         setTimeout(() => words.removeWord(), DURATION);
     }
 }
@@ -42,29 +47,82 @@ export default class HomePage extends Vue {
 <style lang="scss" scoped>
 @import "../assets/styles/variables";
 
-$imageHeight: 1920 / 1371 * 100vh;
+$imageRatio: 1920 / 1371;
+$imageHeight: $imageRatio * 100vh;
+$background-color: #4c483c;
 
 .home {
     position: relative;
     height: 100vh;
-    background: #4c483c url("../assets/images/home/walter.jpg") no-repeat center left;
+    background: $background-color;
+}
+.home-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: url("../assets/images/home/walter.jpg") no-repeat center left;
     background-size: cover;
+
+    @media (max-width: $screen-size-l) {
+        width: calc(100% + calc(#{$imageHeight} / 2.08));
+        left: calc(-#{$imageHeight} / 2.08);
+        max-width: $imageHeight;
+
+        @media (min-aspect-ratio: 960 / 1371) {
+            box-sizing: border-box;
+            box-shadow: 0 0 10vh 10vh $background-color inset;
+        }
+    }
 }
 .word {
+    $padding: 50px;
+    $slide: 50px;
+    $duration: 3s;
+
     position: absolute;
-    font-size: 2em;
     color: white;
+    line-height: 0.8em;
 
-    transition: opacity 2s;
-
+    transition: opacity $duration, transform $duration;
     &.fade-enter,
     &.fade-leave-to {
         opacity: 0;
     }
-}
-@media (max-width: $screen-size-s) {
-    .home {
-        background-position-x: calc(-#{$imageHeight} / 2.08);
+    &.fade-enter {
+        transform: translateX(-$slide);
+    }
+
+    @media (min-width: $screen-size-l) {
+        left: $padding;
+
+        &.right {
+            left: unset;
+            right: $padding;
+            text-align: right;
+
+            &.fade-enter {
+                transform: translateX($slide);
+            }
+        }
+    }
+    @media (max-width: $screen-size-l) {
+        right: $padding;
+        text-align: right;
+
+        &.fade-enter {
+            transform: translateX($slide);
+        }
+    }
+    @media (min-width: $screen-size-s) {
+        font-size: 3em;
+    }
+    @media (max-width: $screen-size-s) {
+        font-size: 2em;
+    }
+    @media (max-width: $screen-size-xs) {
+        font-size: 1.5em;
     }
 }
 </style>

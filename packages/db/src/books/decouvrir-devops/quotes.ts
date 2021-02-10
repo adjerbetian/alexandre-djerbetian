8f25cbdc66318579297f24c95694f79b1b79587f
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2021-02-10 15:32:55 +0100

    Add html formatting

--- packages/db/src/books/decouvrir-devops/quotes.ts
+++ packages/db/src/books/decouvrir-devops/quotes.ts
@@ -1,5 +1,6 @@
 import { QuoteDBModel, Rating } from "../dbModel";
 import { quoteTags } from "../quoteTags";
+import { html } from "../../utils";
 
 export const chapters = ["Introduction", "La démarche DevOps enfin expliquée"];
 
@@ -8,14 +9,16 @@ export const quotes: QuoteDBModel[] = [
         chapter: 1,
         page: "9",
         id: "automatiser-n-est-pas-optimiser",
-        content: `
+        content: html`
             <h3>Automatiser n'est pas optimiser</h3>
             <p>
-                [...] Il est pourtant vrai que l'inconscient collectif associe facilement l'automatisation et les
-                automates à l'industrie et donc à une certaine forme d'optimisation. Une démarche DevOps évite
-                ce type de préjugés. Lorsque nous automatisons une collaboration, nous ne nous embarrassons pas
-                de savoir si elle est optimale et industrialisable, nous nous contentons de nous assurer qu'elle
-                fonctionne.
+                [...] Il est pourtant vrai que l'inconscient collectif associe
+                facilement l'automatisation et les automates à l'industrie et
+                donc à une certaine forme d'optimisation. Une démarche DevOps
+                évite ce type de préjugés. Lorsque nous automatisons une
+                collaboration, nous ne nous embarrassons pas de savoir si elle
+                est optimale et industrialisable, nous nous contentons de nous
+                assurer qu'elle fonctionne.
             </p>
         `,
         tags: [quoteTags.Optimization],
